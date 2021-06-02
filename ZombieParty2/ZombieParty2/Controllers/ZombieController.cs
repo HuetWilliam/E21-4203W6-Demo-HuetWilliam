668307631a0b81f4241a733b9f908085ce07b576
William <2088436@cegepmontpetit.ca>
2021-06-02 16:00:01 -0400

    FCT Création classe "Zombie" et "ZombieController"
    Création de la classe "Zombie" ainsi que la création de son crontroller "ZombieController"

---
+++ ZombieParty2/ZombieParty2/Controllers/ZombieController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZombieParty2.Models;
+
+namespace ZombieParty2.Controllers
+{
+    public class ZombieController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.MaListe = new List<Zombie>()
+            {
+                new Zombie(){Name = "LeChuck", Type = "Fiction", Point = 5},
+                new Zombie(){Name = "Lenore", Type = "Fiction", Point = 4},
+                new Zombie(){Name = "Draugr", Type = "Légendraire", Point = 2},
+                new Zombie(){Name = "Ragamuffin", Type = "Légendraire", Point = 5},
+                new Zombie(){Name = "Taxidermy", Type = "Légendraire", Point = 1},
+                new Zombie(){Name = "chien de l'enfer", Type = "Fiction", Point = 7},
+                new Zombie(){Name = "Avogadro", Type = "Fiction", Point = 9}
+            };
+            
+            return View();
+        }
+    }
+}
