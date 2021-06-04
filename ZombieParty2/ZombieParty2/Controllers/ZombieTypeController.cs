ee252835c31c2f7012cfb74d2e5e52044c018d3d
William <2088436@cegepmontpetit.ca>
2021-06-04 15:34:50 -0400

    FCT Création de la classe "ZombieType" ainsi que deux vues
    Création de la classe ZombieType. Ajout également de la vue "Index" et "Create" (utilisant le controleur "ZombieTypeController")

---
+++ ZombieParty2/ZombieParty2/Controllers/ZombieTypeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZombieParty2.Models;
+
+namespace ZombieParty2.Controllers
+{
+    public class ZombieTypeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.MaListe = new List<ZombieType>()
+            {
+                new ZombieType() {TypeName = "Virus", Id = 1},
+                new ZombieType() {TypeName = "Contact", Id = 2}
+            };
+            
+            return View();
+        }
+
+        //GET CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ ZombieParty2/ZombieParty2/Models/ZombieType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZombieParty2.Models
+{
+    public class ZombieType
+    {
+        public int Id { get; set; }
+        public string TypeName { get; set; }
+    }
+}
